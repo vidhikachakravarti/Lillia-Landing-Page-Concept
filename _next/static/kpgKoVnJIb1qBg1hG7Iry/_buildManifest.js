9cba7e6a4d561b0123e69e73ea8be3c6267bb48a
Vidhika Chakravarti <vidhikachakravarti@Vidhikas-MacBook-Air.local>
2026-04-11 21:35:24 +0530

    Updates

---
+++ _next/static/kpgKoVnJIb1qBg1hG7Iry/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/692-e5ac4e771bf92015.js","static/chunks/pages/index-a12de71fe8d3573b.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
